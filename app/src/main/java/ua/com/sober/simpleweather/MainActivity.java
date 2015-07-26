45c492aed4d0223a5bf96791639c6d0a9fceea8e
Dmitry Hmel <dmitry.hmel@gmail.com>
2015-07-26 23:23:46 +0300

    get json

--- app/src/main/java/ua/com/sober/simpleweather/MainActivity.java
+++ app/src/main/java/ua/com/sober/simpleweather/MainActivity.java
@@ -1,17 +1,29 @@
 package ua.com.sober.simpleweather;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity {
+
+    private static final String jsonUrl = "http://api.openweathermap.org/data/2.5/weather?q=Kiev";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new RequestTask().execute(jsonUrl);
     }
 
     @Override
@@ -23,9 +35,6 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -35,4 +44,36 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class RequestTask extends AsyncTask<String, Void, String> {
+
+        private HttpURLConnection urlConnection = null;
+        private InputStream inputStream = null;
+        private BufferedReader reader = null;
+        private String resultJson = "";
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                inputStream = urlConnection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                resultJson = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return resultJson;
+        }
+
+        @Override
+        protected void onPostExecute(String strJson) {
+            super.onPostExecute(strJson);
+            Log.i("JSON", strJson);
+        }
+    }
+
 }
